a13cd9be3aed1a58c4ef057ef096571b8549dbf2
Ehsan Seyedi <git@ehcan.net>
2025-04-19 12:57:37 +0200

    Add eza configuration and aliases to Fish functions

--- .config/fish/functions/alternatives.fish
+++ .config/fish/functions/alternatives.fish
@@ -3,3 +3,10 @@ alias cat "bat"
 
 # https://github.com/muesli/duf
 alias df "duf"
+
+# https://github.com/eza-community/eza
+set -Ux EZA_CONFIG_DIR ~/.config/eza
+alias ls "eza --icons=always --group-directories-first"
+abbr ll "ls -l"
+abbr la "ls -l -a"
+abbr lt "ls --tree"
