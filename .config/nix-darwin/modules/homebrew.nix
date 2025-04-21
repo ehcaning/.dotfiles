f9eccc8094a460baa746c18d2058e1ef77f5f088
Ehsan Seyedi <git@ehcan.net>
2025-04-21 15:47:13 +0200

    Add Nextdns

--- .config/nix-darwin/modules/homebrew.nix
+++ .config/nix-darwin/modules/homebrew.nix
@@ -11,8 +11,9 @@
       "vlc"
     ];
     masApps = {
-      "The Unarchiver" = 425424353; # https://apps.apple.com/de/app/the-unarchiver/id425424353
       "Enpass" = 732710998; # https://apps.apple.com/de/app/enpass-password-manager/id732710998
+      "Nextdns" = 1464122853; # https://apps.apple.com/de/app/nextdns/id1464122853
+      "The Unarchiver" = 425424353; # https://apps.apple.com/de/app/the-unarchiver/id425424353
     };
     onActivation.autoUpdate = true;
     onActivation.upgrade = true;
