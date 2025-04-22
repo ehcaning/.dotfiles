80f2abcc7be7c9af4bcc9087c408fb6d2b1286f8
Ehsan Seyedi <git@ehcan.net>
2025-04-22 21:33:24 +0200

    Create Raycast scripts

---
+++ .config/raycast/nix-update.sh
@@ -0,0 +1,18 @@
+#!/run/current-system/sw/bin/fish
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template" from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Nix Update
+# @raycast.mode fullOutput
+# @raycast.packageName My Scripts
+#
+# Optional parameters:
+# @raycast.icon https://search.nixos.org/images/nix-logo.png
+
+nix flake update --flake ~/.config/nix-darwin
+darwin-rebuild switch --flake ~/.config/nix-darwin#mac
---
+++ .config/raycast/open-dotfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template" from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Dotfiles
+# @raycast.mode silent
+# @raycast.packageName My Scripts
+#
+# Optional parameters:
+# @raycast.icon 🗃️
+
+code ~/.dotfiles
