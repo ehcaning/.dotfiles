20a5226da52fa980cd90f531529743ddf4dbbe72
Ehsan Seyedi <git@ehcan.net>
2025-06-18 23:47:30 +0200

    Remove slow `ls` alias

--- .config/fish/conf.d/eza.fish
+++ .config/fish/conf.d/eza.fish
@@ -1,5 +1,5 @@
 # https://github.com/eza-community/eza
-alias ls 'eza --icons=always --group-directories-first --color=always --total-size'
+alias ls 'eza --icons=always --group-directories-first --color=always'
 alias ll 'ls -l'
 alias la 'ls -l -a'
 alias llt 'ls --tree'
